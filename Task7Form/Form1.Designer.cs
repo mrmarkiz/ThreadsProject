35acc80b4e930115108a3a1bab46fe5cb13c7b7e
mrmarkiz <maxtomusiak315@gmail.com>
2023-10-18 23:50:32 +0300

    added task7

--- Task7Form/Form1.Designer.cs
+++ Task7Form/Form1.Designer.cs
@@ -28,12 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listBox1 = new ListBox();
+            labelMax = new Label();
+            labelMin = new Label();
+            labelAvg = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(12, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(106, 424);
+            listBox1.TabIndex = 0;
+            // 
+            // labelMax
+            // 
+            labelMax.AutoSize = true;
+            labelMax.Location = new Point(124, 12);
+            labelMax.Name = "labelMax";
+            labelMax.Size = new Size(40, 20);
+            labelMax.TabIndex = 1;
+            labelMax.Text = "Max:";
+            // 
+            // labelMin
+            // 
+            labelMin.AutoSize = true;
+            labelMin.Location = new Point(127, 100);
+            labelMin.Name = "labelMin";
+            labelMin.Size = new Size(37, 20);
+            labelMin.TabIndex = 1;
+            labelMin.Text = "Min:";
+            // 
+            // labelAvg
+            // 
+            labelAvg.AutoSize = true;
+            labelAvg.Location = new Point(127, 182);
+            labelAvg.Name = "labelAvg";
+            labelAvg.Size = new Size(40, 20);
+            labelAvg.TabIndex = 1;
+            labelAvg.Text = "AVG:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(142, 226);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(268, 450);
+            Controls.Add(button1);
+            Controls.Add(labelAvg);
+            Controls.Add(labelMin);
+            Controls.Add(labelMax);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private Label labelMax;
+        private Label labelMin;
+        private Label labelAvg;
+        private Button button1;
     }
 }
--- Task7Form/Form1.cs
+++ Task7Form/Form1.cs
@@ -2,9 +2,54 @@ namespace Task7Form
 {
     public partial class Form1 : Form
     {
+        private int[] arr = new int[10000];
         public Form1()
         {
+            Random rnd = new Random();
             InitializeComponent();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1000000);
+                listBox1.Items.Add(arr[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread threadMax = new Thread(() =>
+            {
+                int max = arr[0];
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    if (arr[i] > max)
+                        max = arr[i];
+                }
+                labelMax.Invoke(() => { labelMax.Text = $"Max: {max}"; });
+            });
+            threadMax.Start();
+
+            Thread threadMin = new Thread(() =>
+            {
+                int min = arr[0];
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    if (arr[i] < min)
+                        min = arr[i];
+                }
+                labelMin.Invoke(() => { labelMin.Text = $"Min: {min}"; });
+            });
+            threadMin.Start();
+
+            Thread threadAvg = new Thread(() =>
+            {
+                int sum = arr[0];
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    sum += arr[i];
+                }
+                labelAvg.Invoke(() => { labelAvg.Text = $"AVG: {(sum) / arr.Length}"; });
+            });
+            threadAvg.Start();
         }
     }
 }
